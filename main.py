5a78d4d36d531b413bd2d6865af1f1d6fba5c3af
RaghavaGVSS <190030489@kluniversity.in>
2022-08-17 10:13:31 +0530

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os, getpass, random
+from datetime import timedelta
+
 import mysql.connector as ms
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, json
 from flask_mail import Message, Mail
 from flask_session import Session
 from validate_email import validate_email
@@ -22,12 +24,24 @@ app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+# app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=1)
 Session(app)
 
 
-@app.route("/welcome", methods=["GET", "POST"])
+@app.before_request
+def make_session_permanent():
+    app.permanent_session_lifetime = timedelta(minutes=10)
+
+
+@app.route("/", methods=["GET", "POST"])
 def wel():
-    return "wel"
+    # url = 'http://freegeoip.net/json/{}'.format(request.remote_addr)
+    # r = request.get(url)
+    # j = json.loads(r.text)
+    # city = j['city']
+    # session['city']=city
+    # print(city)
+    return render_template("wel.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -63,10 +77,11 @@ def log():
                         session["name"] = p[0][0]
                         if not session.get("name"):
                             return redirect("/login")
+
                         hostname = socket.gethostname()
                         ip_address = socket.gethostbyname(hostname)
                         session["hostip"]=ip_address
-                        session["devname"]=hostname
+                        session["devname"]=hostname;session.permanent = True
                         return render_template("user.html", name=p[0][0],data=data,ip=session["hostip"],devnm=session["devname"])
                         # return render_template('stud.html', name=p[0][0], data=data)
                     else:
@@ -84,8 +99,8 @@ def log():
 
         except Exception as e:
             return str(e)
-        finally:
-            cu.close()
+        # finally:
+        #     cu.close()
     # else:return "Method type: "+request.method
     return render_template("login.html")
 
@@ -205,15 +220,22 @@ def adml():
                 if i[0] == email:
                     if i[1] == pswd1:
                         cu.execute("select admlogin.name from admlogin where admlogin.eml=%s", (email,))
-                        print("Right na"*10)
+                        # print("Right na"*10)
 
                         m = cu.fetchall();print("M=",m);session["admname"] = m[0][0]
                         cu.execute("select * from req")
                         r=cu.fetchall()
                         if not session.get("admname"):
                             return redirect("/admlgn")
+                        if session.get("admname") is not None:
+                            session.permanent = True
+                            cu.execute("SELECT COUNT(admlogin.name) FROM rent.admlogin");count = cu.fetchall();print("count=", count)
+                            cu.execute("SELECT COUNT(flats.flat_name) FROM rent.flats");count.append(cu.fetchone());print("count=", count)
+                            cu.execute("SELECT COUNT(userlogin.name) FROM rent.userlogin");count.append(cu.fetchone());print("count3=", count)
+                            cu.execute("SELECT COUNT(req.eml) FROM rent.req");count.append(cu.fetchone());print("count3=", count)
 
-                        return render_template('admdash.html', name=session["admname"] , req=r, labels=labels, values=values)
+                            session["count"] = count
+                            return render_template('admdash.html', name=session["admname"] , req=r, labels=labels, values=values, count=session["count"])
                     else:
                         return "Incorrect Password"
                 else:
@@ -228,83 +250,88 @@ def adml():
 
 @app.route("/admdash", methods=["GET", "POST"])
 def dash():
-    val = request.form.get('nm')
-    data = [
-        ("01-01-2020", 1597),
-        ("02-02-2020", 1456),
-        ("03-03-2020", 1908),
-        ("04-04-2020", 896),
-        ("05-05-2020", 755),
-        ("06-06-2020", 453),
-        ("07-07-2020", 1100),
-        ("08-08-2020", 1235),
-        ("09-09-2020", 1478),
-        ("10-10-2020", 1230),
-        ("11-11-2020", 1540),
-        ("12-12-2020", 1010),
-    ]
-    labels = [row[0] for row in data]
-    values = [row[1] for row in data]
-    try:
-        mydb = ms.connect(host="localhost", user="root", passwd="mysql@!@")
-        cu = mydb.cursor()
-        cu.execute("use rent")
-        cu.execute("select * from req")
-        r = cu.fetchall()
-        cu.execute("select * from req where req.eml=%s",(val,))
-        x=cu.fetchall()
-        print("req",r)
-        if request.form.get('adm') == "Grant Access":
-            print("In adm", val )
-            print("X=",x)
-            email = x[0][0]
-            pin = str(random.randint(10000, 100000))
-            if x:
-                cu.execute("insert into admlogin values(%s,%s,%s,%s)", (x[0][1].strip(), email.strip(), x[0][2], str(pin),))
-                cu.execute("delete from req where req.eml=%s", (email,));mydb.commit()
+    if session.get("admname") is not None:
+        val = request.form.get('nm')
+        data = [
+            ("01-01-2020", 1597),
+            ("02-02-2020", 1456),
+            ("03-03-2020", 1908),
+            ("04-04-2020", 896),
+            ("05-05-2020", 755),
+            ("06-06-2020", 453),
+            ("07-07-2020", 1100),
+            ("08-08-2020", 1235),
+            ("09-09-2020", 1478),
+            ("10-10-2020", 1230),
+            ("11-11-2020", 1540),
+            ("12-12-2020", 1010),
+        ]
+        labels = [row[0] for row in data]
+        values = [row[1] for row in data]
+        try:
+            mydb = ms.connect(host="localhost", user="root", passwd="mysql@!@")
+            cu = mydb.cursor()
+            cu.execute("use rent")
+            cu.execute("select * from req")
+            r = cu.fetchall()
+            cu.execute("select * from req where req.eml=%s",(val,))
+            x=cu.fetchall()
+            print("req",r)
+            if request.form.get('adm') == "Grant Access":
+                print("In adm", val )
+                print("X=",x)
+                email = x[0][0]
+                pin = str(random.randint(10000, 100000))
+                if x:
+                    cu.execute("insert into admlogin values(%s,%s,%s,%s)", (x[0][1].strip(), email.strip(), x[0][2], str(pin),))
+                    cu.execute("delete from req where req.eml=%s", (email,));mydb.commit()
 
-        elif request.form.get('adm1') == "Deny Access":
-            print("Request denied", val )
-            print("X=",x);email = x[0][0]
-            cu.execute("delete from req where req.eml=%s", (email,))
-            mydb.commit()
-            cu.close()
-            # sub = "House Paradox"
-            # msg = Message(
-            #     sub,
-            #     sender='hisir960@gmail.com',
-            #     recipients=[email]
-            # )
-            # msg.body = 'Hi, Thanks for registering for our website.You are a admin now.Please do not share this code with anyone your secret pin is:' + pin
-            # mail.send(msg)
-            print("*" * 30, 'Email Sent')
-        return render_template("admdash.html", labels=labels, values=values, req=r)
+            elif request.form.get('adm1') == "Deny Access":
+                print("Request denied", val )
+                print("X=",x);email = x[0][0]
+                cu.execute("delete from req where req.eml=%s", (email,))
+                mydb.commit()
+                cu.close()
+                # sub = "House Paradox"
+                # msg = Message(
+                #     sub,
+                #     sender='hisir960@gmail.com',
+                #     recipients=[email]
+                # )
+                # msg.body = 'Hi, Thanks for registering for our website.You are a admin now.Please do not share this code with anyone your secret pin is:' + pin
+                # mail.send(msg)
+                print("*" * 30, 'Email Sent')
+            return render_template("admdash.html", labels=labels, values=values, req=r, count=session["count"])
 
-    except Exception as e:
-
-        print(str(e))
-        return redirect(request.referrer)
+        except Exception as e:
+            "Facing error is:"+str(e)
+    else:
+        return redirect("/admlgn")
+    return render_template("admdash.html",count=session["count"])
+        # return redirect(request.referrer)
 
 
 
 @app.route("/usr", methods=["POST", "GET"])
 def usr():
-    try:
-        mydb = ms.connect(host="localhost", user="root", passwd="mysql@!@");
-        cu = mydb.cursor()
-        cu.execute("use rent")
-        cu.execute("select * from flats")
-        data = cu.fetchall()
-        # print("From flats from usr method:",data)
-        cu.close()
-        hostname = socket.gethostname()
-        ip_address = socket.gethostbyname(hostname)
-        session["hostip"] = ip_address
-        session["devname"] = hostname
+    if session.get("name") is not None:
+        try:
+            mydb = ms.connect(host="localhost", user="root", passwd="mysql@!@");
+            cu = mydb.cursor()
+            cu.execute("use rent")
+            cu.execute("select * from flats")
+            data = cu.fetchall()
+            # print("From flats from usr method:",data)
+            cu.close()
+            hostname = socket.gethostname()
+            ip_address = socket.gethostbyname(hostname)
+            session["hostip"] = ip_address
+            session["devname"] = hostname
 
-    except Exception as e:
-        print("Error at:-"+str(e))
-    return render_template("user.html", data=data, name= session["name"],ip=session["hostip"],devnm=session["devname"])
+        except Exception as e:
+            print("Error at:-"+str(e))
+        return render_template("user.html", data=data, name= session["name"],ip=session["hostip"],devnm=session["devname"])
+    else:return redirect("/login")
 
 # fname = request.form['fname']
 #         uname = request.form['uname']
@@ -364,7 +391,7 @@ def house():
 
 
     else:
-        print("Type of mrthod:", request.method)
+        print("Type of method:", request.method)
 
     user_details = {
         'name': os.getlogin(),
@@ -483,32 +510,34 @@ def stds():
 
 @app.route('/regflt', methods=['POST', 'GET'])
 def regflts():
-    val = request.form.get('nm')
-    try:
-        mydb = ms.connect(host="localhost", user="root", passwd="mysql@!@");
-        cu = mydb.cursor()
-        cu.execute("use rent")
-        cu.execute( "select * from flats where flats.flat_name=%s",(val,))
-        data = cu.fetchall()
-        mydb.commit()
-        cu.close()
-        # print("**"*30 , "val=",val)
-        # return render_template("regbk.html",data=data)
-        print("type of data is:", type(data))
-        # if request.method == "post":
-        print("val:-",val);print("data:- ",data)
-        nm1=request.form.get('nm1');print("nm1=",nm1)
-        if request.form.get('submit_button') == "Confirm flat":
-            print("crnm iis:", val );print("data:- ",data)
-            return render_template("payment.html",amt=data[0][8],flt=data[0][0])
+    if session.get("name") is not None:
+        val = request.form.get('nm')
+        try:
+            mydb = ms.connect(host="localhost", user="root", passwd="mysql@!@");
+            cu = mydb.cursor()
+            cu.execute("use rent")
+            cu.execute( "select * from flats where flats.flat_name=%s",(val,))
+            data = cu.fetchall()
+            mydb.commit()
+            cu.close()
+            # print("**"*30 , "val=",val)
+            # return render_template("regbk.html",data=data)
+            print("type of data is:", type(data))
+            # if request.method == "post":
+            print("val:-",val);print("data:- ",data)
+            nm1=request.form.get('nm1');print("nm1=",nm1)
+            if request.form.get('submit_button') == "Confirm flat":
+                print("crnm iis:", val );print("data:- ",data)
+                return render_template("payment.html",amt=data[0][8],flt=data[0][0])
+            return render_template("regflt.html",nm=val, data=data)
+        # "select flats.flat_name,flats.owner,flats.about,flats.owner_phone,flats.rate from flats where flats.flat_name=%s",
+        # (val,)
+        except Exception as e:
+            print("error:-(", str(e))
+
         return render_template("regflt.html",nm=val, data=data)
-    # "select flats.flat_name,flats.owner,flats.about,flats.owner_phone,flats.rate from flats where flats.flat_name=%s",
-    # (val,)
-    except Exception as e:
-        print("error:-(", str(e))
-
-    return render_template("regflt.html",nm=val, data=data)
-
+    else:
+        return redirect("/login")
 @app.route("/viewnew")
 def view():
     try:
@@ -545,31 +574,28 @@ def adding():
     return render_template("add.html", data=[], name="Add")
 
 
-
-
-
-
 @app.route('/del', methods=['POST', 'GET'])
 def delt():
     return render_template("delete.html")
 
 @app.route('/pay', methods=['POST', 'GET'])
 def payt():
-    val = request.form.get('nm1')
-    try:
-        mydb = ms.connect(host="localhost", user="root", passwd="mysql@!@");
-        cu = mydb.cursor()
-        cu.execute("use rent")
-        cu.execute("select * from flats where flats.flat_name=%s", (val,))
-        data = cu.fetchall()
-        mydb.commit()
-        cu.close()
-        print("nm1 from pay:",val)
+    if session.get("name") is not None:
+        val = request.form.get('nm1')
+        try:
+            mydb = ms.connect(host="localhost", user="root", passwd="mysql@!@");
+            cu = mydb.cursor()
+            cu.execute("use rent")
+            cu.execute("select * from flats where flats.flat_name=%s", (val,))
+            data = cu.fetchall()
+            mydb.commit()
+            cu.close()
+            print("nm1 from pay:",val)
 
-        return render_template("payment.html",amt=data[0][8],flt=data[0][0])
-    except Exception as e:return "Error is at:"+str(e)
+            return render_template("payment.html",amt=data[0][8],flt=data[0][0])
+        except Exception as e:return "Error is at:"+str(e)
 
-    # return render_template("payment.html")
+    else:return redirect("/login")
 
 @app.route("/logout")
 def logout():
@@ -582,3 +608,29 @@ def logout1():
     session["admname"] = None
     session["hostip"] = None
     return redirect("/admlgn")
+
+@app.route("/frgt", methods=['POST', 'GET'])
+def frg():
+    if session.get("admname") is not None:
+        if request.method == "POST":
+            eml = request.form.get('eml')
+            try:
+                mydb = ms.connect(host="localhost", user="root", passwd="mysql@!@");
+                cu = mydb.cursor()
+                cu.execute("use rent")
+                cu.execute("select admlogin.name from admlogin where admlogin.eml=%s",(eml,))
+                #           select userlogin.eml from userlogin where userlogin.eml = % s", (email,)
+                lst=cu.fetchall()
+                print("lst=",lst)
+                if list is not None:
+                    print("Send email")
+                    return redirect("/admdash")
+
+            except Exception as e: print("Error from here is:" + str(e))
+
+        else:print("Method is of type: ",request.method)
+        return render_template("frgtpswd.html")
+    else:
+        return redirect("/admlgn")
+# clicking send btn posting the info and refreshing the page automatically
+
